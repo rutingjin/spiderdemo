b2fa4077d80e4e426da69f49f0e8be895b967e66
rutingjin <rutingjin@renrenche.com>
2019-11-11 19:29:57 +0800

    :beers: Support SSR link decoding

---
+++ src/adapter/source.onessr.ts
@@ -0,0 +1,34 @@
+import request from 'request'
+import { decodeSSRLink } from '../utils'
+
+const uri = 'https://onessr.ml/articles/getArticles'
+
+const formData = {
+    offset: 0,
+    pageSize: 4,
+}
+
+export function fetchSSRList ():Promise<SSRNode[]> {
+    return new Promise<SSRNode[]>((resolve, reject) => {
+        request({
+            uri,
+            method: 'POST',
+            formData,
+            json: true
+        }, (error, response, body) => {
+            if (error) {
+                reject()
+            }
+            const originString = body.data && body.data[0] && body.data[0].articleContent || ''
+            const linkList = originString
+                .split('\n')
+                .map((line: string) => {
+                    if (line) {
+                        let reg:RegExp = /(?<=>)[^<>]+(?=<)/g
+                        return line.match(reg)[0]
+                    }
+                })
+            resolve(decodeSSRLink(linkList))
+        })
+    })
+}
