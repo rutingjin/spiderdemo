4d9d75e99369904b1fd99582124c00c502e3b2f5
rutingjin <rutingjin@renrenche.com>
2019-11-11 12:46:51 +0800

    testing: :white_check_mark: Add test data for program usability testing

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-import filter from './core'
+import Filter from './core'
+import { readFileSync } from 'fs'
+import path from 'path'
 
+
+const nodes: SSRNode[] = JSON.parse(
+    readFileSync(
+        path.resolve(__dirname, '../data/testdata.json')
+    ).toString()
+)
+
+const filter = new Filter(nodes)
+
+filter.check().then(data => {
+    console.log(data)
+})
