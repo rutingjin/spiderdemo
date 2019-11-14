d2c2f86ddabf3e992ee5f86fc0134cac4153245a
rutingjin <rutingjin@renrenche.com>
2019-11-14 11:00:27 +0800

    Improve program  :checked_flag:	 compatibility

--- src/utils/overrideConfig.ts
+++ src/utils/overrideConfig.ts
@@ -1,26 +1,31 @@
 import path from "path";
 import {readFile, writeFile} from "fs";
+import { platformEnum } from './enum'
 
-const filePath = path.resolve(__dirname, '../../bin/win32/gui-config.json')
-
-export default function (node: SSRNode):Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-        readFile(filePath, (err, data) => {
-            if (err) {
-                reject(new Error('An error occurred while reading the config file.'))
-            }
-            let SSRConfig = JSON.parse(data.toString())
-            SSRConfig.configs = [node]
-            writeFile(
-                filePath,
-                JSON.stringify(SSRConfig, null, 2),
-                (err) => {
-                    if (err) {
-                        reject(new Error('An error occurred while writing the config file.'))
-                    }
-                    resolve()
+export default function (node: SSRNode):Promise<proxyConfig> {
+    return new Promise<proxyConfig>((resolve, reject) => {
+        // Compatible with windows platform
+        if (process.platform === platformEnum.window) {
+            const filePath = path.resolve(__dirname, '../../bin/win32/gui-config.json')
+            readFile(filePath, (err, data) => {
+                if (err) {
+                    reject(new Error('An error occurred while reading the config file.'))
                 }
-            )
-        })
+                let SSRConfig = JSON.parse(data.toString())
+                SSRConfig.configs = [node]
+                writeFile(
+                    filePath,
+                    JSON.stringify(SSRConfig, null, 2),
+                    (err) => {
+                        if (err) {
+                            reject(new Error('An error occurred while writing the config file.'))
+                        }
+                        resolve({ proxy: `http://127.0.0.1:${SSRConfig.localPort}` })
+                    }
+                )
+            })
+        }
+        // TODO: Other systems are not yet compatible
+        reject(new Error('Only support windows platform, other systems are not yet compatible'))
     })
 }
